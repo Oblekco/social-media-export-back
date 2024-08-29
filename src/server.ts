5af443c08f42b96a647f899b7c5f2efe145bc921
Juan Sterren <sterrenjuanignacio@gmail.com>
2024-08-29 10:16:49 -0300

    📝 add README.md

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.use(
     cors({
         origin: 'http://localhost:5173',
         methods: ['GET', 'POST'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
+        allowedHeaders: ['Content-Type', 'Authorization']
     })
 )
 
