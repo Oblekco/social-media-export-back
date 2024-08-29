0bcba5947c95b9c6014b9eed715c15a9f8b4067e
Juan Sterren <sterrenjuanignacio@gmail.com>
2024-08-29 15:50:34 -0300

    📬 generate xlsx file and send attachment

--- src/routes/exportSocialDataRoutes.ts
+++ src/routes/exportSocialDataRoutes.ts
@@ -5,6 +5,6 @@ const router = express.Router()
 import { authenticateToken } from '../middlewares/authenticateToken'
 import { exportSocialData } from '../controllers/exportSocialDataController'
 
-router.get('/export/mysql-to-xlsx', authenticateToken, exportSocialData)
+router.post('/export/mysql-to-xlsx', authenticateToken, exportSocialData)
 
 export default router
